f58c608c6947ae194ff6db8cd9a11da43112d1e9
Jacky Efendi <jacky.efendi1@gmail.com>
2021-03-24 22:00:44 +0700

    Center content

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -2,6 +2,16 @@ import { Container } from "@chakra-ui/react"
 
 export const Layout: React.FC = ({ children }) => {
   return (
-    <Container py={4} maxW="container.md" overflowX="hidden">{children}</Container>
+    <Container
+      py={4}
+      maxW="container.md"
+      overflowX="hidden"
+      minH="90vh"
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+    >
+      {children}
+    </Container>
   )
 }
