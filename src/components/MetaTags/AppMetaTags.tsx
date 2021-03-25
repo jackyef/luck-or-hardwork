63c122cc8bcb0fd0ae0d0c6545ded0d3e5c4de99
jackyef <jacky.efendi1@gmail.com>
2021-03-25 10:37:40 +0700

    Add favicons

--- src/components/MetaTags/AppMetaTags.tsx
+++ src/components/MetaTags/AppMetaTags.tsx
@@ -1,12 +1,11 @@
-import { theme } from '@/theme/theme'
+import { theme } from '@/theme/theme';
 import { useRouter } from 'next/router';
-import Head from 'next/head'
+import Head from 'next/head';
 
 export const publicUrl = 'https://luck-or-hardwork.vercel.app';
 
 export const defaultTitle = 'Is success determined by luck 🍀 or hard work 🛠️?';
-const defaultDescription =
-  `Some people think that it's their hardwork that led them to their success. Others think that luck is the one that led to one's success.`;
+const defaultDescription = `Some people think that it's their hardwork that led them to their success. Others think that luck is the one that led to one's success.`;
 const defaultOgImage = 'https://luck-or-hardwork.vercel.app/og-macos.png';
 
 interface Props {
@@ -14,7 +13,6 @@ interface Props {
   title?: string;
   description?: string;
 }
-
 
 export const AppMetaTags: React.FC<Props> = ({
   image = defaultOgImage,
@@ -30,6 +28,24 @@ export const AppMetaTags: React.FC<Props> = ({
       <title>{title}</title>
       <meta name="description" content={description} />
       <link rel="canonical" href={url} />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/favicons/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicons/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicons/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/favicons/site.webmanifest" />
 
       {/* Facebook OG meta tags */}
       <meta property="og:url" content={url} />
@@ -75,5 +91,5 @@ export const AppMetaTags: React.FC<Props> = ({
         }}
       />
     </Head>
-  )
-}
+  );
+};
